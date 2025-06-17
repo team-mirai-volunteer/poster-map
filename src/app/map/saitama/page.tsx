d8418c4f80da733689c351f277a23a09a65ae0bc
auco67 <auco67@gmail.com>
2025-06-17 20:39:38 +0900

    update: 埼玉県の市区町村リンクを追加

--- src/app/map/saitama/page.tsx
+++ src/app/map/saitama/page.tsx
@@ -18,26 +18,221 @@ export default function saitamaPage() {
                 <Link href="/map/saitama/all" className="list-group-item list-group-item-action">
                     埼玉県全域
                 </Link>
-                <Link href="/map/saitama?block=23-city" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/saitama/all?block=saitama-nishi" className="list-group-item list-group-item-action">
+                    さいたま市西区
                 </Link>
-                <Link href="/map/saitama?block=23-east" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/saitama/all?block=saitama-kita" className="list-group-item list-group-item-action">
+                    さいたま市北区
                 </Link>
-                <Link href="/map/saitama?block=23-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/saitama/all?block=saitama-omiya" className="list-group-item list-group-item-action">
+                    さいたま市大宮区
                 </Link>
-                <Link href="/map/saitama?block=tama-north" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/saitama/all?block=saitama-minuma" className="list-group-item list-group-item-action">
+                    さいたま市見沼区
                 </Link>
-                <Link href="/map/saitama?block=tama-south" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/saitama/all?block=saitama-chuo" className="list-group-item list-group-item-action">
+                    さいたま市中央区
                 </Link>
-                <Link href="/map/saitama?block=tama-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/saitama/all?block=saitama-sakura" className="list-group-item list-group-item-action">
+                    さいたま市桜区
                 </Link>
-                <Link href="/map/saitama?block=island" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/saitama/all?block=saitama-urawa" className="list-group-item list-group-item-action">
+                    さいたま市浦和区
+                </Link>
+                <Link href="/map/saitama/all?block=saitama-minami" className="list-group-item list-group-item-action">
+                    さいたま市南区
+                </Link>
+                <Link href="/map/saitama/all?block=saitama-midori" className="list-group-item list-group-item-action">
+                    さいたま市緑区
+                </Link>
+                <Link href="/map/saitama/all?block=saitama-iwatsuki" className="list-group-item list-group-item-action">
+                    さいたま市岩槻区
+                </Link>
+                <Link href="/map/saitama/all?block=kawagoe" className="list-group-item list-group-item-action">
+                    川越市
+                </Link>
+                <Link href="/map/saitama/all?block=kumagaya" className="list-group-item list-group-item-action">
+                    熊谷市
+                </Link>
+                <Link href="/map/saitama/all?block=kawaguchi" className="list-group-item list-group-item-action">
+                    川口市
+                </Link>
+                <Link href="/map/saitama/all?block=gyoda" className="list-group-item list-group-item-action">
+                    行田市
+                </Link>
+                <Link href="/map/saitama/all?block=chichibu" className="list-group-item list-group-item-action">
+                    秩父市
+                </Link>
+                <Link href="/map/saitama/all?block=tokorozawa" className="list-group-item list-group-item-action">
+                    所沢市
+                </Link>
+                <Link href="/map/saitama/all?block=hanno" className="list-group-item list-group-item-action">
+                    飯能市
+                </Link>
+                <Link href="/map/saitama/all?block=kazo" className="list-group-item list-group-item-action">
+                    加須市
+                </Link>
+                <Link href="/map/saitama/all?block=honjo" className="list-group-item list-group-item-action">
+                    本庄市
+                </Link>
+                <Link href="/map/saitama/all?block=higashimatsuyama" className="list-group-item list-group-item-action">
+                    東松山市
+                </Link>
+                <Link href="/map/saitama/all?block=kasukabe" className="list-group-item list-group-item-action">
+                    春日部市
+                </Link>
+                <Link href="/map/saitama/all?block=sayama" className="list-group-item list-group-item-action">
+                    狭山市
+                </Link>
+                <Link href="/map/saitama/all?block=hanyu" className="list-group-item list-group-item-action">
+                    羽生市
+                </Link>
+                <Link href="/map/saitama/all?block=konosu" className="list-group-item list-group-item-action">
+                    鴻巣市
+                </Link>
+                <Link href="/map/saitama/all?block=fukaya" className="list-group-item list-group-item-action">
+                    深谷市
+                </Link>
+                <Link href="/map/saitama/all?block=ageo" className="list-group-item list-group-item-action">
+                    上尾市
+                </Link>
+                <Link href="/map/saitama/all?block=soka" className="list-group-item list-group-item-action">
+                    草加市
+                </Link>
+                <Link href="/map/saitama/all?block=koshigaya" className="list-group-item list-group-item-action">
+                    越谷市
+                </Link>
+                <Link href="/map/saitama/all?block=warabi" className="list-group-item list-group-item-action">
+                    蕨市
+                </Link>
+                <Link href="/map/saitama/all?block=toda" className="list-group-item list-group-item-action">
+                    戸田市
+                </Link>
+                <Link href="/map/saitama/all?block=iruma" className="list-group-item list-group-item-action">
+                    入間市
+                </Link>
+                <Link href="/map/saitama/all?block=asaka" className="list-group-item list-group-item-action">
+                    朝霞市
+                </Link>
+                <Link href="/map/saitama/all?block=shiki" className="list-group-item list-group-item-action">
+                    志木市
+                </Link>
+                <Link href="/map/saitama/all?block=wako" className="list-group-item list-group-item-action">
+                    和光市
+                </Link>
+                <Link href="/map/saitama/all?block=niiza" className="list-group-item list-group-item-action">
+                    新座市
+                </Link>
+                <Link href="/map/saitama/all?block=okegawa" className="list-group-item list-group-item-action">
+                    桶川市
+                </Link>
+                <Link href="/map/saitama/all?block=kuki" className="list-group-item list-group-item-action">
+                    久喜市
+                </Link>
+                <Link href="/map/saitama/all?block=kitamoto" className="list-group-item list-group-item-action">
+                    北本市
+                </Link>
+                <Link href="/map/saitama/all?block=yashio" className="list-group-item list-group-item-action">
+                    八潮市
+                </Link>
+                <Link href="/map/saitama/all?block=fujimi" className="list-group-item list-group-item-action">
+                    富士見市
+                </Link>
+                <Link href="/map/saitama/all?block=misato" className="list-group-item list-group-item-action">
+                    三郷市
+                </Link>
+                <Link href="/map/saitama/all?block=hasuda" className="list-group-item list-group-item-action">
+                    蓮田市
+                </Link>
+                <Link href="/map/saitama/all?block=sakado" className="list-group-item list-group-item-action">
+                    坂戸市
+                </Link>
+                <Link href="/map/saitama/all?block=satte" className="list-group-item list-group-item-action">
+                    幸手市
+                </Link>
+                <Link href="/map/saitama/all?block=tsurugashima" className="list-group-item list-group-item-action">
+                    鶴ヶ島市
+                </Link>
+                <Link href="/map/saitama/all?block=hidaka" className="list-group-item list-group-item-action">
+                    日高市
+                </Link>
+                <Link href="/map/saitama/all?block=yoshikawa" className="list-group-item list-group-item-action">
+                    吉川市
+                </Link>
+                <Link href="/map/saitama/all?block=fujimino" className="list-group-item list-group-item-action">
+                    ふじみ野市
+                </Link>
+                <Link href="/map/saitama/all?block=shiraoka" className="list-group-item list-group-item-action">
+                    白岡市
+                </Link>
+                <Link href="/map/saitama/all?block=ina" className="list-group-item list-group-item-action">
+                    伊奈町
+                </Link>
+                <Link href="/map/saitama/all?block=miyoshi" className="list-group-item list-group-item-action">
+                    三芳町
+                </Link>
+                <Link href="/map/saitama/all?block=moroyama" className="list-group-item list-group-item-action">
+                    毛呂山町
+                </Link>
+                <Link href="/map/saitama/all?block=ogose" className="list-group-item list-group-item-action">
+                    越生町
+                </Link>
+                <Link href="/map/saitama/all?block=namegawa" className="list-group-item list-group-item-action">
+                    滑川町
+                </Link>
+                <Link href="/map/saitama/all?block=ranzan" className="list-group-item list-group-item-action">
+                    嵐山町
+                </Link>
+                <Link href="/map/saitama/all?block=ogawa" className="list-group-item list-group-item-action">
+                    小川町
+                </Link>
+                <Link href="/map/saitama/all?block=kawajima" className="list-group-item list-group-item-action">
+                    川島町
+                </Link>
+                <Link href="/map/saitama/all?block=yoshimi" className="list-group-item list-group-item-action">
+                    吉見町
+                </Link>
+                <Link href="/map/saitama/all?block=hatoyama" className="list-group-item list-group-item-action">
+                    鳩山町
+                </Link>
+                <Link href="/map/saitama/all?block=tokigawa" className="list-group-item list-group-item-action">
+                    ときがわ町
+                </Link>
+                <Link href="/map/saitama/all?block=yokoze" className="list-group-item list-group-item-action">
+                    横瀬町
+                </Link>
+                <Link href="/map/saitama/all?block=minano" className="list-group-item list-group-item-action">
+                    皆野町
+                </Link>
+                <Link href="/map/saitama/all?block=nagatoro" className="list-group-item list-group-item-action">
+                    長瀞町
+                </Link>
+                <Link href="/map/saitama/all?block=ogano" className="list-group-item list-group-item-action">
+                    小鹿野町
+                </Link>
+                <Link href="/map/saitama/all?block=higashichichibu" className="list-group-item list-group-item-action">
+                    東秩父村
+                </Link>
+                <Link href="/map/saitama/all?block=misato_machi" className="list-group-item list-group-item-action">
+                    美里町
+                </Link>
+                <Link href="/map/saitama/all?block=kamikawa" className="list-group-item list-group-item-action">
+                    神川町
+                </Link>
+                <Link href="/map/saitama/all?block=kamisato" className="list-group-item list-group-item-action">
+                    上里町
+                </Link>
+                <Link href="/map/saitama/all?block=yorii" className="list-group-item list-group-item-action">
+                    寄居町
+                </Link>
+                <Link href="/map/saitama/all?block=miyashiro" className="list-group-item list-group-item-action">
+                    宮代町
+                </Link>
+                <Link href="/map/saitama/all?block=sugito" className="list-group-item list-group-item-action">
+                    杉戸町
+                </Link>
+                <Link href="/map/saitama/all?block=matsubushi" className="list-group-item list-group-item-action">
+                    松伏町
                 </Link>
             </div>
             <div>
