085b4599474796a83e339e01ff91c538a14a72e6
auco67 <auco67@gmail.com>
2025-06-17 18:44:57 +0900

    update: 愛知県の市区町村リンクを追加

--- src/app/map/aichi/page.tsx
+++ src/app/map/aichi/page.tsx
@@ -18,26 +18,167 @@ export default function AichiPage() {
                 <Link href="/map/aichi/all" className="list-group-item list-group-item-action">
                     愛知県全域
                 </Link>
-                <Link href="/map/aichi?block=23-city" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/aichi/all?block=nagoya" className="list-group-item list-group-item-action">
+                    名古屋市
                 </Link>
-                <Link href="/map/aichi?block=23-east" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/aichi/all?block=toyohashi" className="list-group-item list-group-item-action">
+                    豊橋市
                 </Link>
-                <Link href="/map/aichi?block=23-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/aichi/all?block=okazaki" className="list-group-item list-group-item-action">
+                    岡崎市
                 </Link>
-                <Link href="/map/aichi?block=tama-north" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/aichi/all?block=ichinomiya" className="list-group-item list-group-item-action">
+                    一宮市
                 </Link>
-                <Link href="/map/aichi?block=tama-south" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/aichi/all?block=seto" className="list-group-item list-group-item-action">
+                    瀬戸市
                 </Link>
-                <Link href="/map/aichi?block=tama-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/aichi/all?block=handa" className="list-group-item list-group-item-action">
+                    半田市
                 </Link>
-                <Link href="/map/aichi?block=island" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/aichi/all?block=kasugai" className="list-group-item list-group-item-action">
+                    春日井市
+                </Link>
+                <Link href="/map/aichi/all?block=toyokawa" className="list-group-item list-group-item-action">
+                    豊川市
+                </Link>
+                <Link href="/map/aichi/all?block=tsushima" className="list-group-item list-group-item-action">
+                    津島市
+                </Link>
+                <Link href="/map/aichi/all?block=hekinan" className="list-group-item list-group-item-action">
+                    碧南市
+                </Link>
+                <Link href="/map/aichi/all?block=kariya" className="list-group-item list-group-item-action">
+                    刈谷市
+                </Link>
+                <Link href="/map/aichi/all?block=oyota" className="list-group-item list-group-item-action">
+                    豊田市
+                </Link>
+                <Link href="/map/aichi/all?block=anjo" className="list-group-item list-group-item-action">
+                    安城市
+                </Link>
+                <Link href="/map/aichi/all?block=nishio" className="list-group-item list-group-item-action">
+                    西尾市
+                </Link>
+                <Link href="/map/aichi/all?block=gamagori" className="list-group-item list-group-item-action">
+                    蒲郡市
+                </Link>
+                <Link href="/map/aichi/all?block=inuyama" className="list-group-item list-group-item-action">
+                    犬山市
+                </Link>
+                <Link href="/map/aichi/all?block=tokoname" className="list-group-item list-group-item-action">
+                    常滑市
+                </Link>
+                <Link href="/map/aichi/all?block=konan" className="list-group-item list-group-item-action">
+                    江南市
+                </Link>
+                <Link href="/map/aichi/all?block=komaki" className="list-group-item list-group-item-action">
+                    小牧市
+                </Link>
+                <Link href="/map/aichi/all?block=inazawa" className="list-group-item list-group-item-action">
+                    稲沢市
+                </Link>
+                <Link href="/map/aichi/all?block=shinshiro" className="list-group-item list-group-item-action">
+                    新城市
+                </Link>
+                <Link href="/map/aichi/all?block=tokai" className="list-group-item list-group-item-action">
+                    東海市
+                </Link>
+                <Link href="/map/aichi/all?block=obu" className="list-group-item list-group-item-action">
+                    大府市
+                </Link>
+                <Link href="/map/aichi/all?block=chita" className="list-group-item list-group-item-action">
+                    知多市
+                </Link>
+                <Link href="/map/aichi/all?block=chiryu" className="list-group-item list-group-item-action">
+                    知立市
+                </Link>
+                <Link href="/map/aichi/all?block=owariasahi" className="list-group-item list-group-item-action">
+                    尾張旭市
+                </Link>
+                <Link href="/map/aichi/all?block=takahama" className="list-group-item list-group-item-action">
+                    高浜市
+                </Link>
+                <Link href="/map/aichi/all?block=iwakura" className="list-group-item list-group-item-action">
+                    岩倉市
+                </Link>
+                <Link href="/map/aichi/all?block=toyoake" className="list-group-item list-group-item-action">
+                    豊明市
+                </Link>
+                <Link href="/map/aichi/all?block=nisshin" className="list-group-item list-group-item-action">
+                    日進市
+                </Link>
+                <Link href="/map/aichi/all?block=tahara" className="list-group-item list-group-item-action">
+                    田原市
+                </Link>
+                <Link href="/map/aichi/all?block=aisai" className="list-group-item list-group-item-action">
+                    愛西市
+                </Link>
+                <Link href="/map/aichi/all?block=kiyosu" className="list-group-item list-group-item-action">
+                    清須市
+                </Link>
+                <Link href="/map/aichi/all?block=kitanagoya" className="list-group-item list-group-item-action">
+                    北名古屋市
+                </Link>
+                <Link href="/map/aichi/all?block=yatomi" className="list-group-item list-group-item-action">
+                    弥富市
+                </Link>
+                <Link href="/map/aichi/all?block=miyoshi" className="list-group-item list-group-item-action">
+                    みよし市
+                </Link>
+                <Link href="/map/aichi/all?block=ama" className="list-group-item list-group-item-action">
+                    あま市
+                </Link>
+                <Link href="/map/aichi/all?block=nagakute" className="list-group-item list-group-item-action">
+                    長久手市
+                </Link>
+                <Link href="/map/aichi/all?block=togo" className="list-group-item list-group-item-action">
+                    愛知郡東郷町
+                </Link>
+                <Link href="/map/aichi/all?block=toyoyama" className="list-group-item list-group-item-action">
+                    西春日井郡豊山町
+                </Link>
+                <Link href="/map/aichi/all?block=oguchi" className="list-group-item list-group-item-action">
+                    丹羽郡大口町
+                </Link>
+                <Link href="/map/aichi/all?block=fuso" className="list-group-item list-group-item-action">
+                    丹羽郡扶桑町
+                </Link>
+                <Link href="/map/aichi/all?block=ofuji" className="list-group-item list-group-item-action">
+                    海部郡大治町
+                </Link>
+                <Link href="/map/aichi/all?block=kanie" className="list-group-item list-group-item-action">
+                    海部郡蟹江町
+                </Link>
+                <Link href="/map/aichi/all?block=tobishima" className="list-group-item list-group-item-action">
+                    海部郡飛島村
+                </Link>
+                <Link href="/map/aichi/all?block=agui" className="list-group-item list-group-item-action">
+                    知多郡阿久比町
+                </Link>
+                <Link href="/map/aichi/all?block=higashiura" className="list-group-item list-group-item-action">
+                    知多郡東浦町
+                </Link>
+                <Link href="/map/aichi/all?block=minamichita" className="list-group-item list-group-item-action">
+                    知多郡南知多町
+                </Link>
+                <Link href="/map/aichi/all?block=mihama" className="list-group-item list-group-item-action">
+                    知多郡美浜町
+                </Link>
+                <Link href="/map/aichi/all?block=taketoyo" className="list-group-item list-group-item-action">
+                    知多郡武豊町
+                </Link>
+                <Link href="/map/aichi/all?block=kota" className="list-group-item list-group-item-action">
+                    額田郡幸田町
+                </Link>
+                <Link href="/map/aichi/all?block=shitara" className="list-group-item list-group-item-action">
+                    北設楽郡設楽町
+                </Link>
+                <Link href="/map/aichi/all?block=toei" className="list-group-item list-group-item-action">
+                    北設楽郡東栄町
+                </Link>
+                <Link href="/map/aichi/all?block=toyone" className="list-group-item list-group-item-action">
+                    北設楽郡豊根村
                 </Link>
             </div>
             <div>
