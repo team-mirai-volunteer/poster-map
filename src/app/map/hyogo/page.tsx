8a5e009f3a4edc27a6bf80b5df108b140ed3ca4c
auco67 <auco67@gmail.com>
2025-06-18 13:43:24 +0900

    update:兵庫県 の市区町村リンクを追加

--- src/app/map/hyogo/page.tsx
+++ src/app/map/hyogo/page.tsx
@@ -18,26 +18,128 @@ export default function HyogoPage() {
                 <Link href="/map/hyogo/all" className="list-group-item list-group-item-action">
                     兵庫県全域
                 </Link>
-                <Link href="/map/hyogo?block=23-city" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/hyogo/all?block=kobe" className="list-group-item list-group-item-action">
+                    神戸市
                 </Link>
-                <Link href="/map/hyogo?block=23-east" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/hyogo/all?block=himeji" className="list-group-item list-group-item-action">
+                    姫路市
                 </Link>
-                <Link href="/map/hyogo?block=23-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/hyogo/all?block=amagasaki" className="list-group-item list-group-item-action">
+                    尼崎市
                 </Link>
-                <Link href="/map/hyogo?block=tama-north" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/hyogo/all?block=akashi" className="list-group-item list-group-item-action">
+                    明石市
                 </Link>
-                <Link href="/map/hyogo?block=tama-south" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/hyogo/all?block=nishinomiya" className="list-group-item list-group-item-action">
+                    西宮市
                 </Link>
-                <Link href="/map/hyogo?block=tama-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/hyogo/all?block=sumoto" className="list-group-item list-group-item-action">
+                    洲本市
                 </Link>
-                <Link href="/map/hyogo?block=island" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/hyogo/all?block=ashiya" className="list-group-item list-group-item-action">
+                    芦屋市
+                </Link>
+                <Link href="/map/hyogo/all?block=itami" className="list-group-item list-group-item-action">
+                    伊丹市
+                </Link>
+                <Link href="/map/hyogo/all?block=aioi" className="list-group-item list-group-item-action">
+                    相生市
+                </Link>
+                <Link href="/map/hyogo/all?block=toyooka" className="list-group-item list-group-item-action">
+                    豊岡市
+                </Link>
+                <Link href="/map/hyogo/all?block=kakogawa" className="list-group-item list-group-item-action">
+                    加古川市
+                </Link>
+                <Link href="/map/hyogo/all?block=ako" className="list-group-item list-group-item-action">
+                    赤穂市
+                </Link>
+                <Link href="/map/hyogo/all?block=nishiwaki" className="list-group-item list-group-item-action">
+                    西脇市
+                </Link>
+                <Link href="/map/hyogo/all?block=takarazuka" className="list-group-item list-group-item-action">
+                    宝塚市
+                </Link>
+                <Link href="/map/hyogo/all?block=miki" className="list-group-item list-group-item-action">
+                    三木市
+                </Link>
+                <Link href="/map/hyogo/all?block=takasago" className="list-group-item list-group-item-action">
+                    高砂市
+                </Link>
+                <Link href="/map/hyogo/all?block=kawanishi" className="list-group-item list-group-item-action">
+                    川西市
+                </Link>
+                <Link href="/map/hyogo/all?block=ono" className="list-group-item list-group-item-action">
+                    小野市
+                </Link>
+                <Link href="/map/hyogo/all?block=sanda" className="list-group-item list-group-item-action">
+                    三田市
+                </Link>
+                <Link href="/map/hyogo/all?block=kasai" className="list-group-item list-group-item-action">
+                    加西市
+                </Link>
+                <Link href="/map/hyogo/all?block=tambasasayama" className="list-group-item list-group-item-action">
+                    丹波篠山市
+                </Link>
+                <Link href="/map/hyogo/all?block=yabu" className="list-group-item list-group-item-action">
+                    養父市
+                </Link>
+                <Link href="/map/hyogo/all?block=tamba" className="list-group-item list-group-item-action">
+                    丹波市
+                </Link>
+                <Link href="/map/hyogo/all?block=minamiawaji" className="list-group-item list-group-item-action">
+                    南あわじ市
+                </Link>
+                <Link href="/map/hyogo/all?block=asago" className="list-group-item list-group-item-action">
+                    朝来市
+                </Link>
+                <Link href="/map/hyogo/all?block=awaji" className="list-group-item list-group-item-action">
+                    淡路市
+                </Link>
+                <Link href="/map/hyogo/all?block=shiso" className="list-group-item list-group-item-action">
+                    宍粟市
+                </Link>
+                <Link href="/map/hyogo/all?block=kato" className="list-group-item list-group-item-action">
+                    加東市
+                </Link>
+                <Link href="/map/hyogo/all?block=tatsuno" className="list-group-item list-group-item-action">
+                    たつの市
+                </Link>
+                <Link href="/map/hyogo/all?block=inagawa" className="list-group-item list-group-item-action">
+                    猪名川町
+                </Link>
+                <Link href="/map/hyogo/all?block=taka" className="list-group-item list-group-item-action">
+                    多可町
+                </Link>
+                <Link href="/map/hyogo/all?block=inami" className="list-group-item list-group-item-action">
+                    稲美町
+                </Link>
+                <Link href="/map/hyogo/all?block=harima" className="list-group-item list-group-item-action">
+                    播磨町
+                </Link>
+                <Link href="/map/hyogo/all?block=ichikawa" className="list-group-item list-group-item-action">
+                    市川町
+                </Link>
+                <Link href="/map/hyogo/all?block=fukusaki" className="list-group-item list-group-item-action">
+                    福崎町
+                </Link>
+                <Link href="/map/hyogo/all?block=kamikawa" className="list-group-item list-group-item-action">
+                    神河町
+                </Link>
+                <Link href="/map/hyogo/all?block=taishi" className="list-group-item list-group-item-action">
+                    太子町
+                </Link>
+                <Link href="/map/hyogo/all?block=kamigori" className="list-group-item list-group-item-action">
+                    上郡町
+                </Link>
+                <Link href="/map/hyogo/all?block=sayo" className="list-group-item list-group-item-action">
+                    佐用町
+                </Link>
+                <Link href="/map/hyogo/all?block=kami" className="list-group-item list-group-item-action">
+                    香美町
+                </Link>
+                <Link href="/map/hyogo/all?block=shinonsen" className="list-group-item list-group-item-action">
+                    新温泉町
                 </Link>
             </div>
             <div>
