7290f7e20151511386dd2f5df406ff1be71d9c11
auco67 <auco67@gmail.com>
2025-06-18 13:45:38 +0900

    update: 長野県の市区町村リンクを追加

--- src/app/map/nagano/page.tsx
+++ src/app/map/nagano/page.tsx
@@ -18,26 +18,236 @@ export default function NaganoPage() {
                 <Link href="/map/nagano/all" className="list-group-item list-group-item-action">
                     長野県全域
                 </Link>
-                <Link href="/map/nagano?block=23-city" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/nagano/all?block=nagano" className="list-group-item list-group-item-action">
+                    長野市
                 </Link>
-                <Link href="/map/nagano?block=23-east" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/nagano/all?block=matsumoto" className="list-group-item list-group-item-action">
+                    松本市
                 </Link>
-                <Link href="/map/nagano?block=23-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/nagano/all?block=ueda" className="list-group-item list-group-item-action">
+                    上田市
                 </Link>
-                <Link href="/map/nagano?block=tama-north" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/nagano/all?block=okaya" className="list-group-item list-group-item-action">
+                    岡谷市
                 </Link>
-                <Link href="/map/nagano?block=tama-south" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/nagano/all?block=iida" className="list-group-item list-group-item-action">
+                    飯田市
                 </Link>
-                <Link href="/map/nagano?block=tama-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/nagano/all?block=suwa" className="list-group-item list-group-item-action">
+                    諏訪市
                 </Link>
-                <Link href="/map/nagano?block=island" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/nagano/all?block=suzaka" className="list-group-item list-group-item-action">
+                    須坂市
+                </Link>
+                <Link href="/map/nagano/all?block=komoro" className="list-group-item list-group-item-action">
+                    小諸市
+                </Link>
+                <Link href="/map/nagano/all?block=ina" className="list-group-item list-group-item-action">
+                    伊那市
+                </Link>
+                <Link href="/map/nagano/all?block=komagane" className="list-group-item list-group-item-action">
+                    駒ヶ根市
+                </Link>
+                <Link href="/map/nagano/all?block=nakano" className="list-group-item list-group-item-action">
+                    中野市
+                </Link>
+                <Link href="/map/nagano/all?block=omachi" className="list-group-item list-group-item-action">
+                    大町市
+                </Link>
+                <Link href="/map/nagano/all?block=iiyama" className="list-group-item list-group-item-action">
+                    飯山市
+                </Link>
+                <Link href="/map/nagano/all?block=chino" className="list-group-item list-group-item-action">
+                    茅野市
+                </Link>
+                <Link href="/map/nagano/all?block=shiojiri" className="list-group-item list-group-item-action">
+                    塩尻市
+                </Link>
+                <Link href="/map/nagano/all?block=saku" className="list-group-item list-group-item-action">
+                    佐久市
+                </Link>
+                <Link href="/map/nagano/all?block=chikuma" className="list-group-item list-group-item-action">
+                    千曲市
+                </Link>
+                <Link href="/map/nagano/all?block=tomi" className="list-group-item list-group-item-action">
+                    東御市
+                </Link>
+                <Link href="/map/nagano/all?block=azumino" className="list-group-item list-group-item-action">
+                    安曇野市
+                </Link>
+                <Link href="/map/nagano/all?block=koumi" className="list-group-item list-group-item-action">
+                    小海町
+                </Link>
+                <Link href="/map/nagano/all?block=kawakami" className="list-group-item list-group-item-action">
+                    川上村
+                </Link>
+                <Link href="/map/nagano/all?block=minamimaki" className="list-group-item list-group-item-action">
+                    南牧村
+                </Link>
+                <Link href="/map/nagano/all?block=minamiaiki" className="list-group-item list-group-item-action">
+                    南相木村
+                </Link>
+                <Link href="/map/nagano/all?block=kitaaiki" className="list-group-item list-group-item-action">
+                    北相木村
+                </Link>
+                <Link href="/map/nagano/all?block=sakuho" className="list-group-item list-group-item-action">
+                    佐久穂町
+                </Link>
+                <Link href="/map/nagano/all?block=karuizawa" className="list-group-item list-group-item-action">
+                    軽井沢町
+                </Link>
+                <Link href="/map/nagano/all?block=miyota" className="list-group-item list-group-item-action">
+                    御代田町
+                </Link>
+                <Link href="/map/nagano/all?block=tateshina" className="list-group-item list-group-item-action">
+                    立科町
+                </Link>
+                <Link href="/map/nagano/all?block=aoki" className="list-group-item list-group-item-action">
+                    青木村
+                </Link>
+                <Link href="/map/nagano/all?block=nagawa" className="list-group-item list-group-item-action">
+                    長和町
+                </Link>
+                <Link href="/map/nagano/all?block=shimosuwa" className="list-group-item list-group-item-action">
+                    下諏訪町
+                </Link>
+                <Link href="/map/nagano/all?block=fujimi" className="list-group-item list-group-item-action">
+                    富士見町
+                </Link>
+                <Link href="/map/nagano/all?block=hara" className="list-group-item list-group-item-action">
+                    原村
+                </Link>
+                <Link href="/map/nagano/all?block=tatsuno" className="list-group-item list-group-item-action">
+                    辰野町
+                </Link>
+                <Link href="/map/nagano/all?block=minowa" className="list-group-item list-group-item-action">
+                    箕輪町
+                </Link>
+                <Link href="/map/nagano/all?block=iijima" className="list-group-item list-group-item-action">
+                    飯島町
+                </Link>
+                <Link href="/map/nagano/all?block=minamiminowa" className="list-group-item list-group-item-action">
+                    南箕輪村
+                </Link>
+                <Link href="/map/nagano/all?block=nakagawa" className="list-group-item list-group-item-action">
+                    中川村
+                </Link>
+                <Link href="/map/nagano/all?block=miyada" className="list-group-item list-group-item-action">
+                    宮田村
+                </Link>
+                <Link href="/map/nagano/all?block=matsukawa" className="list-group-item list-group-item-action">
+                    松川町
+                </Link>
+                <Link href="/map/nagano/all?block=takamori" className="list-group-item list-group-item-action">
+                    高森町
+                </Link>
+                <Link href="/map/nagano/all?block=anan" className="list-group-item list-group-item-action">
+                    阿南町
+                </Link>
+                <Link href="/map/nagano/all?block=achi" className="list-group-item list-group-item-action">
+                    阿智村
+                </Link>
+                <Link href="/map/nagano/all?block=hiraya" className="list-group-item list-group-item-action">
+                    平谷村
+                </Link>
+                <Link href="/map/nagano/all?block=neba" className="list-group-item list-group-item-action">
+                    根羽村
+                </Link>
+                <Link href="/map/nagano/all?block=shimojo" className="list-group-item list-group-item-action">
+                    下條村
+                </Link>
+                <Link href="/map/nagano/all?block=urugi" className="list-group-item list-group-item-action">
+                    売木村
+                </Link>
+                <Link href="/map/nagano/all?block=tenryu" className="list-group-item list-group-item-action">
+                    天龍村
+                </Link>
+                <Link href="/map/nagano/all?block=yasuoka" className="list-group-item list-group-item-action">
+                    泰阜村
+                </Link>
+                <Link href="/map/nagano/all?block=takagi" className="list-group-item list-group-item-action">
+                    喬木村
+                </Link>
+                <Link href="/map/nagano/all?block=toyooka" className="list-group-item list-group-item-action">
+                    豊丘村
+                </Link>
+                <Link href="/map/nagano/all?block=ooshika" className="list-group-item list-group-item-action">
+                    大鹿村
+                </Link>
+                <Link href="/map/nagano/all?block=agematsu" className="list-group-item list-group-item-action">
+                    上松町
+                </Link>
+                <Link href="/map/nagano/all?block=nagiso" className="list-group-item list-group-item-action">
+                    南木曽町
+                </Link>
+                <Link href="/map/nagano/all?block=kiso-mura" className="list-group-item list-group-item-action">
+                    木祖村
+                </Link>
+                <Link href="/map/nagano/all?block=otaki" className="list-group-item list-group-item-action">
+                    王滝村
+                </Link>
+                <Link href="/map/nagano/all?block=okuwa" className="list-group-item list-group-item-action">
+                    大桑村
+                </Link>
+                <Link href="/map/nagano/all?block=kiso" className="list-group-item list-group-item-action">
+                    木曽町
+                </Link>
+                <Link href="/map/nagano/all?block=omi" className="list-group-item list-group-item-action">
+                    麻績村
+                </Link>
+                <Link href="/map/nagano/all?block=ikusaka" className="list-group-item list-group-item-action">
+                    生坂村
+                </Link>
+                <Link href="/map/nagano/all?block=yamagata" className="list-group-item list-group-item-action">
+                    山形村
+                </Link>
+                <Link href="/map/nagano/all?block=asahi" className="list-group-item list-group-item-action">
+                    朝日村
+                </Link>
+                <Link href="/map/nagano/all?block=chikuhoku" className="list-group-item list-group-item-action">
+                    筑北村
+                </Link>
+                <Link href="/map/nagano/all?block=ikeda" className="list-group-item list-group-item-action">
+                    池田町
+                </Link>
+                <Link href="/map/nagano/all?block=matsukawa-mura" className="list-group-item list-group-item-action">
+                    松川村
+                </Link>
+                <Link href="/map/nagano/all?block=hakuba" className="list-group-item list-group-item-action">
+                    白馬村
+                </Link>
+                <Link href="/map/nagano/all?block=otari" className="list-group-item list-group-item-action">
+                    小谷村
+                </Link>
+                <Link href="/map/nagano/all?block=sakaki" className="list-group-item list-group-item-action">
+                    坂城町
+                </Link>
+                <Link href="/map/nagano/all?block=obuse" className="list-group-item list-group-item-action">
+                    小布施町
+                </Link>
+                <Link href="/map/nagano/all?block=takayama" className="list-group-item list-group-item-action">
+                    高山村
+                </Link>
+                <Link href="/map/nagano/all?block=yamanouchi" className="list-group-item list-group-item-action">
+                    山ノ内町
+                </Link>
+                <Link href="/map/nagano/all?block=kijimadaira" className="list-group-item list-group-item-action">
+                    木島平村
+                </Link>
+                <Link href="/map/nagano/all?block=nozawaonsen" className="list-group-item list-group-item-action">
+                    野沢温泉村
+                </Link>
+                <Link href="/map/nagano/all?block=shinano" className="list-group-item list-group-item-action">
+                    信濃町
+                </Link>
+                <Link href="/map/nagano/all?block=ogawa" className="list-group-item list-group-item-action">
+                    小川村
+                </Link>
+                <Link href="/map/nagano/all?block=iizuna" className="list-group-item list-group-item-action">
+                    飯綱町
+                </Link>
+                <Link href="/map/nagano/all?block=sakae" className="list-group-item list-group-item-action">
+                    栄村
                 </Link>
             </div>
             <div>
