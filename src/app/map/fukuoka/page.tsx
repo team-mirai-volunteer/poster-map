9b80e8e58b59167c72676171def2ec9d1ce6c487
auco67 <auco67@gmail.com>
2025-06-18 13:41:23 +0900

    update:福岡県の市区町村リンクを追加

--- src/app/map/fukuoka/page.tsx
+++ src/app/map/fukuoka/page.tsx
@@ -18,26 +18,185 @@ export default function FukuokaPage() {
                 <Link href="/map/fukuoka/all" className="list-group-item list-group-item-action">
                     福岡県全域
                 </Link>
-                <Link href="/map/fukuoka?block=23-city" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/fukuoka/all?block=kitakyushu" className="list-group-item list-group-item-action">
+                    北九州市
                 </Link>
-                <Link href="/map/fukuoka?block=23-east" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/fukuoka/all?block=fukuoka" className="list-group-item list-group-item-action">
+                    福岡市
                 </Link>
-                <Link href="/map/fukuoka?block=23-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/fukuoka/all?block=omuta" className="list-group-item list-group-item-action">
+                    大牟田市
                 </Link>
-                <Link href="/map/fukuoka?block=tama-north" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/fukuoka/all?block=kurume" className="list-group-item list-group-item-action">
+                    久留米市
                 </Link>
-                <Link href="/map/fukuoka?block=tama-south" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/fukuoka/all?block=nogata" className="list-group-item list-group-item-action">
+                    直方市
                 </Link>
-                <Link href="/map/fukuoka?block=tama-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/fukuoka/all?block=iizuka" className="list-group-item list-group-item-action">
+                    飯塚市
                 </Link>
-                <Link href="/map/fukuoka?block=island" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/fukuoka/all?block=tagawa" className="list-group-item list-group-item-action">
+                    田川市
+                </Link>
+                <Link href="/map/fukuoka/all?block=yanagawa" className="list-group-item list-group-item-action">
+                    柳川市
+                </Link>
+                <Link href="/map/fukuoka/all?block=yame" className="list-group-item list-group-item-action">
+                    八女市
+                </Link>
+                <Link href="/map/fukuoka/all?block=chikugo" className="list-group-item list-group-item-action">
+                    筑後市
+                </Link>
+                <Link href="/map/fukuoka/all?block=okawa" className="list-group-item list-group-item-action">
+                    大川市
+                </Link>
+                <Link href="/map/fukuoka/all?block=yukuhashi" className="list-group-item list-group-item-action">
+                    行橋市
+                </Link>
+                <Link href="/map/fukuoka/all?block=buzen" className="list-group-item list-group-item-action">
+                    豊前市
+                </Link>
+                <Link href="/map/fukuoka/all?block=nakama" className="list-group-item list-group-item-action">
+                    中間市
+                </Link>
+                <Link href="/map/fukuoka/all?block=ogori" className="list-group-item list-group-item-action">
+                    小郡市
+                </Link>
+                <Link href="/map/fukuoka/all?block=chikushino" className="list-group-item list-group-item-action">
+                    筑紫野市
+                </Link>
+                <Link href="/map/fukuoka/all?block=kasuga" className="list-group-item list-group-item-action">
+                    春日市
+                </Link>
+                <Link href="/map/fukuoka/all?block=onojo" className="list-group-item list-group-item-action">
+                    大野城市
+                </Link>
+                <Link href="/map/fukuoka/all?block=munakata" className="list-group-item list-group-item-action">
+                    宗像市
+                </Link>
+                <Link href="/map/fukuoka/all?block=dazaifu" className="list-group-item list-group-item-action">
+                    太宰府市
+                </Link>
+                <Link href="/map/fukuoka/all?block=koga" className="list-group-item list-group-item-action">
+                    古賀市
+                </Link>
+                <Link href="/map/fukuoka/all?block=fukutsu" className="list-group-item list-group-item-action">
+                    福津市
+                </Link>
+                <Link href="/map/fukuoka/all?block=ukiha" className="list-group-item list-group-item-action">
+                    うきは市
+                </Link>
+                <Link href="/map/fukuoka/all?block=miyawaka" className="list-group-item list-group-item-action">
+                    宮若市
+                </Link>
+                <Link href="/map/fukuoka/all?block=kama" className="list-group-item list-group-item-action">
+                    嘉麻市
+                </Link>
+                <Link href="/map/fukuoka/all?block=asakura" className="list-group-item list-group-item-action">
+                    朝倉市
+                </Link>
+                <Link href="/map/fukuoka/all?block=miyama" className="list-group-item list-group-item-action">
+                    みやま市
+                </Link>
+                <Link href="/map/fukuoka/all?block=itoshima" className="list-group-item list-group-item-action">
+                    糸島市
+                </Link>
+                <Link href="/map/fukuoka/all?block=nakagawa" className="list-group-item list-group-item-action">
+                    那珂川市
+                </Link>
+                <Link href="/map/fukuoka/all?block=umi" className="list-group-item list-group-item-action">
+                    宇美町
+                </Link>
+                <Link href="/map/fukuoka/all?block=sasaguri" className="list-group-item list-group-item-action">
+                    篠栗町
+                </Link>
+                <Link href="/map/fukuoka/all?block=shime" className="list-group-item list-group-item-action">
+                    志免町
+                </Link>
+                <Link href="/map/fukuoka/all?block=sue" className="list-group-item list-group-item-action">
+                    須惠町
+                </Link>
+                <Link href="/map/fukuoka/all?block=shingu" className="list-group-item list-group-item-action">
+                    新宮町
+                </Link>
+                <Link href="/map/fukuoka/all?block=hisayama" className="list-group-item list-group-item-action">
+                    久山町
+                </Link>
+                <Link href="/map/fukuoka/all?block=kasuya" className="list-group-item list-group-item-action">
+                    粕屋町
+                </Link>
+                <Link href="/map/fukuoka/all?block=ashiya" className="list-group-item list-group-item-action">
+                    芦屋町
+                </Link>
+                <Link href="/map/fukuoka/all?block=mizumaki" className="list-group-item list-group-item-action">
+                    水巻町
+                </Link>
+                <Link href="/map/fukuoka/all?block=okagaki" className="list-group-item list-group-item-action">
+                    岡垣町
+                </Link>
+                <Link href="/map/fukuoka/all?block=onga" className="list-group-item list-group-item-action">
+                    遠賀町
+                </Link>
+                <Link href="/map/fukuoka/all?block=otakemachi" className="list-group-item list-group-item-action">
+                    小竹町
+                </Link>
+                <Link href="/map/fukuoka/all?block=kurate" className="list-group-item list-group-item-action">
+                    鞍手町
+                </Link>
+                <Link href="/map/fukuoka/all?block=keisen" className="list-group-item list-group-item-action">
+                    桂川町
+                </Link>
+                <Link href="/map/fukuoka/all?block=chikuzen" className="list-group-item list-group-item-action">
+                    筑前町
+                </Link>
+                <Link href="/map/fukuoka/all?block=toho" className="list-group-item list-group-item-action">
+                    東峰村
+                </Link>
+                <Link href="/map/fukuoka/all?block=otachiarai" className="list-group-item list-group-item-action">
+                    大刀洗町
+                </Link>
+                <Link href="/map/fukuoka/all?block=oki" className="list-group-item list-group-item-action">
+                    大木町
+                </Link>
+                <Link href="/map/fukuoka/all?block=hirokawa" className="list-group-item list-group-item-action">
+                    広川町
+                </Link>
+                <Link href="/map/fukuoka/all?block=kawara" className="list-group-item list-group-item-action">
+                    香春町
+                </Link>
+                <Link href="/map/fukuoka/all?block=soeda" className="list-group-item list-group-item-action">
+                    添田町
+                </Link>
+                <Link href="/map/fukuoka/all?block=itoda" className="list-group-item list-group-item-action">
+                    糸田町
+                </Link>
+                <Link href="/map/fukuoka/all?block=kawasaki" className="list-group-item list-group-item-action">
+                    川崎町
+                </Link>
+                <Link href="/map/fukuoka/all?block=otomo" className="list-group-item list-group-item-action">
+                    大任町
+                </Link>
+                <Link href="/map/fukuoka/all?block=aka" className="list-group-item list-group-item-action">
+                    赤村
+                </Link>
+                <Link href="/map/fukuoka/all?block=fukuchi" className="list-group-item list-group-item-action">
+                    福智町
+                </Link>
+                <Link href="/map/fukuoka/all?block=kanda" className="list-group-item list-group-item-action">
+                    苅田町
+                </Link>
+                <Link href="/map/fukuoka/all?block=miyako" className="list-group-item list-group-item-action">
+                    みやこ町
+                </Link>
+                <Link href="/map/fukuoka/all?block=yoshitomi" className="list-group-item list-group-item-action">
+                    吉富町
+                </Link>
+                <Link href="/map/fukuoka/all?block=koge" className="list-group-item list-group-item-action">
+                    上毛町
+                </Link>
+                <Link href="/map/fukuoka/all?block=chikujo" className="list-group-item list-group-item-action">
+                    築上町
                 </Link>
             </div>
             <div>
