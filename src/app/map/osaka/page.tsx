3d86558cba3c22863f3ef15f6a0db935164a83e9
auco67 <auco67@gmail.com>
2025-06-18 13:46:27 +0900

    update: 大阪府の市区町村リンクを追加

--- src/app/map/osaka/page.tsx
+++ src/app/map/osaka/page.tsx
@@ -18,26 +18,134 @@ export default function OsakaPage() {
                 <Link href="/map/osaka/all" className="list-group-item list-group-item-action">
                     大阪府全域
                 </Link>
-                <Link href="/map/osaka?block=23-city" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/osaka/all?block=osaka" className="list-group-item list-group-item-action">
+                    大阪市
                 </Link>
-                <Link href="/map/osaka?block=23-east" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/osaka/all?block=sakai" className="list-group-item list-group-item-action">
+                    堺市
                 </Link>
-                <Link href="/map/osaka?block=23-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/osaka/all?block=kishiwada" className="list-group-item list-group-item-action">
+                    岸和田市
                 </Link>
-                <Link href="/map/osaka?block=tama-north" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/osaka/all?block=toyonaka" className="list-group-item list-group-item-action">
+                    豊中市
                 </Link>
-                <Link href="/map/osaka?block=tama-south" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/osaka/all?block=ikeda" className="list-group-item list-group-item-action">
+                    池田市
                 </Link>
-                <Link href="/map/osaka?block=tama-west" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/osaka/all?block=suita" className="list-group-item list-group-item-action">
+                    吹田市
                 </Link>
-                <Link href="/map/osaka?block=island" className="list-group-item list-group-item-action">
-                    
+                <Link href="/map/osaka/all?block=izumiotsu" className="list-group-item list-group-item-action">
+                    泉大津市
+                </Link>
+                <Link href="/map/osaka/all?block=takatsuki" className="list-group-item list-group-item-action">
+                    高槻市
+                </Link>
+                <Link href="/map/osaka/all?block=kaizuka" className="list-group-item list-group-item-action">
+                    貝塚市
+                </Link>
+                <Link href="/map/osaka/all?block=moriguchi" className="list-group-item list-group-item-action">
+                    守口市
+                </Link>
+                <Link href="/map/osaka/all?block=hirakata" className="list-group-item list-group-item-action">
+                    枚方市
+                </Link>
+                <Link href="/map/osaka/all?block=ibaraki" className="list-group-item list-group-item-action">
+                    茨木市
+                </Link>
+                <Link href="/map/osaka/all?block=yao" className="list-group-item list-group-item-action">
+                    八尾市
+                </Link>
+                <Link href="/map/osaka/all?block=izumisano" className="list-group-item list-group-item-action">
+                    泉佐野市
+                </Link>
+                <Link href="/map/osaka/all?block=tondabayashi" className="list-group-item list-group-item-action">
+                    富田林市
+                </Link>
+                <Link href="/map/osaka/all?block=neyagawa" className="list-group-item list-group-item-action">
+                    寝屋川市
+                </Link>
+                <Link href="/map/osaka/all?block=kawachinagano" className="list-group-item list-group-item-action">
+                    河内長野市
+                </Link>
+                <Link href="/map/osaka/all?block=matsubara" className="list-group-item list-group-item-action">
+                    松原市
+                </Link>
+                <Link href="/map/osaka/all?block=daito" className="list-group-item list-group-item-action">
+                    大東市
+                </Link>
+                <Link href="/map/osaka/all?block=izumi" className="list-group-item list-group-item-action">
+                    和泉市
+                </Link>
+                <Link href="/map/osaka/all?block=minoh" className="list-group-item list-group-item-action">
+                    箕面市
+                </Link>
+                <Link href="/map/osaka/all?block=kashiwara" className="list-group-item list-group-item-action">
+                    柏原市
+                </Link>
+                <Link href="/map/osaka/all?block=habikino" className="list-group-item list-group-item-action">
+                    羽曳野市
+                </Link>
+                <Link href="/map/osaka/all?block=kadoma" className="list-group-item list-group-item-action">
+                    門真市
+                </Link>
+                <Link href="/map/osaka/all?block=settsu" className="list-group-item list-group-item-action">
+                    摂津市
+                </Link>
+                <Link href="/map/osaka/all?block=takaishi" className="list-group-item list-group-item-action">
+                    高石市
+                </Link>
+                <Link href="/map/osaka/all?block=fujiidera" className="list-group-item list-group-item-action">
+                    藤井寺市
+                </Link>
+                <Link href="/map/osaka/all?block=higashiosaka" className="list-group-item list-group-item-action">
+                    東大阪市
+                </Link>
+                <Link href="/map/osaka/all?block=sennan" className="list-group-item list-group-item-action">
+                    泉南市
+                </Link>
+                <Link href="/map/osaka/all?block=shijonawate" className="list-group-item list-group-item-action">
+                    四條畷市
+                </Link>
+                <Link href="/map/osaka/all?block=katano" className="list-group-item list-group-item-action">
+                    交野市
+                </Link>
+                <Link href="/map/osaka/all?block=osakasayama" className="list-group-item list-group-item-action">
+                    大阪狭山市
+                </Link>
+                <Link href="/map/osaka/all?block=hannan" className="list-group-item list-group-item-action">
+                    阪南市
+                </Link>
+                <Link href="/map/osaka/all?block=shimamoto" className="list-group-item list-group-item-action">
+                    島本町
+                </Link>
+                <Link href="/map/osaka/all?block=toyono" className="list-group-item list-group-item-action">
+                    豊能町
+                </Link>
+                <Link href="/map/osaka/all?block=nose" className="list-group-item list-group-item-action">
+                    能勢町
+                </Link>
+                <Link href="/map/osaka/all?block=tadaoka" className="list-group-item list-group-item-action">
+                    忠岡町
+                </Link>
+                <Link href="/map/osaka/all?block=kumatori" className="list-group-item list-group-item-action">
+                    熊取町
+                </Link>
+                <Link href="/map/osaka/all?block=tajiri" className="list-group-item list-group-item-action">
+                    田尻町
+                </Link>
+                <Link href="/map/osaka/all?block=misaki" className="list-group-item list-group-item-action">
+                    岬町
+                </Link>
+                <Link href="/map/osaka/all?block=taishi" className="list-group-item list-group-item-action">
+                    太子町
+                </Link>
+                <Link href="/map/osaka/all?block=kanan" className="list-group-item list-group-item-action">
+                    河南町
+                </Link>
+                <Link href="/map/osaka/all?block=chihayaakasaka" className="list-group-item list-group-item-action">
+                    千早赤阪村
                 </Link>
             </div>
             <div>
