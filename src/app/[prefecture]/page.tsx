6da5dd4c53efa53f95a8a39361ba05b98c34b5e0
auco67 <auco67@gmail.com>
2025-06-23 11:16:00 +0900

    update: 都道府県マップへ構成変更

--- src/app/[prefecture]/page.tsx
+++ src/app/[prefecture]/page.tsx
@@ -1,73 +1,24 @@
-import Link from 'next/link';
-import { notFound } from 'next/navigation';
-import { getPrefectureConfig, getAllPrefectures } from '@/lib/prefecture-config';
-
-export async function generateStaticParams() {
-  const prefectures = getAllPrefectures();
-  return prefectures.map((prefecture) => ({
-    prefecture: prefecture.id,
-  }));
-}
+import MapClient from '@/app/[prefecture]/MapClient'
+import { loadPrefectureData } from '@/lib/server-data';
+import { getAllPrefectures } from '@/lib/prefecture-config';
 
 interface PageProps {
-  params: Promise<{
+    params: Promise<{
     prefecture: string;
-  }>;
+    }>;
 }
 
-export default async function PrefecturePage({ params }: PageProps) {
-  const { prefecture } = await params;
-  const prefectureConfig = getPrefectureConfig(prefecture);
+export default async function MapPage({ params }: PageProps) {
+    const { prefecture } = await params;
+    const data = await loadPrefectureData(prefecture);
+    return (
+    <MapClient prefecture={prefecture} prefectureData={data} />
+    );
+}
 
-  if (!prefectureConfig) {
-    notFound();
-  }
-
-  const { nameJa, blocks } = prefectureConfig;
-
-  return (
-    <div className="container-sm my-5">
-      <div className="col-lg-12 px-0">
-        <h3 className="mb-4">チームみらいマップ {nameJa}</h3>
-        <div>
-          <a 
-            href="/"
-            rel="noopener noreferrer" 
-            className="link-secondary"
-          >
-            都道府県へ戻る
-          </a>
-        </div>
-
-        <div className="list-group mb-4 mt-4">
-          <Link 
-            href={`/${prefecture}/all`} 
-            className="list-group-item list-group-item-action"
-          >
-            {nameJa}全域
-          </Link>
-          
-          {blocks && blocks.map((block) => (
-            <Link 
-              key={block.id}
-              href={`/${prefecture}/all?block=${block.id}`} 
-              className="list-group-item list-group-item-action"
-            >
-              {block.name}
-            </Link>
-          ))}
-        </div>
-
-        <div>
-          <a 
-            href="/"
-            rel="noopener noreferrer" 
-            className="link-secondary"
-          >
-            都道府県へ戻る
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+export async function generateStaticParams() {
+    const prefectures = getAllPrefectures();
+    return prefectures.map((prefecture) => ({
+        prefecture: prefecture.id,
+    }));
 }
